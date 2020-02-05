48e9caee5059f2e06d28838b8079575cc468d08d
simpsons01 <39054010+simpsons01@users.noreply.github.com>
2020-02-05 23:58:35 +0800

    add env variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express()
 const path = require('path')
+require('dotenv').config();
 
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/index.html'))
+    res.render(path.resolve(__dirname + '/index.ejs'),{api:process.env.api_key})
 });
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
