2edf4146e9341f6692c71673b464d27a6cca22d1
simpsons01 <39054010+simpsons01@users.noreply.github.com>
2020-02-07 07:54:11 +0800

    add-google-map

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require('dotenv-safe').config();
+require('dotenv').config()
 const express = require('express');
 const app = express()
 const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.render(path.resolve(__dirname + '/index.ejs'),{api:process.env.api_key})
+    res.render(path.resolve(__dirname + '/index.ejs'),{api_key:process.env.api_key})
 });
 
 app.listen(3000, function () {
