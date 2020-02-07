ca03f29bb42a6ff59675f2e315f7fb3e825e78d1
simpsons01 <ilikekobe01@gmail.com>
2020-02-07 17:35:49 +0800

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.render(path.resolve(__dirname + '/index.ejs'),{api_key:process.env.api_key})
